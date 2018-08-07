73dff55066f60cd248fc09231e3be2415dd6dae1
Ana Vazquez <ana.maria.vl@hotmail.com>
2018-08-06 21:10:42 -0500

    Agregando html de verificacion

--- src/socialtime.js
+++ src/socialtime.js
@@ -7,12 +7,18 @@ function initMap(){
     let map = new
     google.maps.Map(document.getElementById('map'), options);
 
-    let marker = new
+    //Click mapa
+    google.maps.event.addListener(map, 'click', function(event){
+        //Agregando marcadores
+        addMarker({coords: event.latLng});
+    })
+
+    /*let marker = new
     google.maps.Marker({
         position: {lat: 19.427, lng: -99.1677,},
         map:map,
-        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    });
+        
+    });*/
 
     let infoWindow = new google.maps.infoWindow({
         content: '<h1>CDMX</h1>'
@@ -20,5 +26,54 @@ function initMap(){
 
     marker.addListener('click', function(){
         infoWindow.open(map, marker);
-    })
+    });
+
+    //arr markers
+    let markers = [
+        {
+            coords: {lat: 19.427, lng: -99.1677,},
+            iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            content: '<h1>CDMX</h1>'
+        },
+        {
+            coords: {lat: 19.427, lng: -99.1677,}
+            },
+            {
+                coords: {lat: 19.427, lng: -99.1677,}
+            }
+    ];
+
+    for(let i = 0; i > markers.length; i ++){
+        addMarker(markers[i]);
+    }
+
+    addMarker({
+        coords: {lat: 19.427, lng: -99.1677,},
+        iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content: '<h1>CDMX</h1>'
+    });
+    addMarker({coords: {lat: 19.427, lng: -99.1677,}});
+    addMarker({coords: {lat: 19.427, lng: -99.1677,}});
+
+    function addMarker(coords){
+        let marker = new google.maps.Marker({
+            position: props.coords,
+            map:map,
+            //icon: props.iconImage,
+        });
+
+        if(props.iconImage){
+            marker.setIcon(props.iconImage);
+        }
+
+        if(props.content){
+            let infoWindow = new google.maps.infoWindow({
+                content: props.content,
+            });
+        
+            marker.addListener('click', function(){
+                infoWindow.open(map, marker);
+            });
+        }
+    }
 }
