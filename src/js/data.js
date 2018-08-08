3eac5873c9f709b6505692b2c19049c8aad5cfc4
Ana Vazquez <ana.maria.vl@hotmail.com>
2018-08-07 23:40:18 -0500

    Modificando diseno y agregando data

---
+++ src/js/data.js
@@ -0,0 +1,11 @@
+//Objeto global
+window.data =  {
+    mexicana: {
+      name: 'El Bajio',
+      direction: ' Av. Cuitláhuac 2709, Obrero Popular, 02840 Ciudad de México, CDMX',
+      },
+      japonesa: {
+        name: 'Tori Tori',
+        direction: 'Temístocles 61, Polanco IV Sección, 11550 Miguel Hidalgo, CDMX',
+      }
+    };
--- src/js/socialtime.js
+++ src/js/socialtime.js
@@ -1,18 +1,18 @@
 function initMap(){
-    let options = {//Opciones del mapa
-        zoom: 5,
-        center: {lat: 19.2542, lng: -99.0739,}
-    }
     //Nuevo mapa
     let map = new
-    google.maps.Map(document.getElementById('map'), options);
+    google.maps.Map(document.getElementById('map'), {
+        center: {lat: 19.2542, lng: -99.0739},
+        zoom:16
+    });
+    
 
     //Click mapa
     google.maps.event.addListener(map, 'click', function(event){
         //Agregando marcadores
         addMarker({coords: event.latLng});
-    })
-
+    });
+};
     /*let marker = new
     google.maps.Marker({
         position: {lat: 19.427, lng: -99.1677,},
@@ -20,14 +20,12 @@ function initMap(){
         
     });*/
 
-    let infoWindow = new google.maps.infoWindow({
+    /*let infoWindow = new google.maps.infoWindow({
         content: '<h1>CDMX</h1>'
     });
-
     marker.addListener('click', function(){
         infoWindow.open(map, marker);
     });
-
     //arr markers
     let markers = [
         {
@@ -42,11 +40,9 @@ function initMap(){
                 coords: {lat: 19.427, lng: -99.1677,}
             }
     ];
-
     for(let i = 0; i > markers.length; i ++){
         addMarker(markers[i]);
     }
-
     addMarker({
         coords: {lat: 19.427, lng: -99.1677,},
         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
@@ -54,18 +50,15 @@ function initMap(){
     });
     addMarker({coords: {lat: 19.427, lng: -99.1677,}});
     addMarker({coords: {lat: 19.427, lng: -99.1677,}});
-}
     function addMarker(coords){
         let marker = new google.maps.Marker({
             position: props.coords,
             map:map,
             //icon: props.iconImage,
         });
-
         if(props.iconImage){
             marker.setIcon(props.iconImage);
         }
-
         if(props.content){
             let infoWindow = new google.maps.infoWindow({
                 content: props.content,
@@ -76,3 +69,22 @@ function initMap(){
             });
         }
     }
+*/
+let inSearch = document.getElementById("search").value;
+let btnSearch = document.getElementById("buscar");
+let paint = document.getElementById("inPaint");
+
+btnSearch.addEventListener("click", event => {
+    if(inSearch == 'mexicana'){
+        paint.innerHTML = `
+        <h1>Nombre: ${data.mexicana.name}</h1>
+        <p>Direccion: ${data.mexicana.direction}</p>
+        `
+    }else(inSearch == 'japonesa') {
+        paint.innerHTML = `
+        <h1>Nombre: ${data.japonesa.name}</h1>
+        <p>Direccion: ${data.japonesa.direction}</p>
+        `
+    }
+});
+
